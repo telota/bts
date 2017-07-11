023e2b0e8b21f26887abcd24a421a4ec81c66431
Sebastian Götte <bbaw@jaseg.net>
2017-07-11 15:37:08 +0200

    Prettify OS detection logic

--- org.bbaw.bts.commons/src/org/bbaw/bts/commons/OSValidator.java
+++ org.bbaw.bts.commons/src/org/bbaw/bts/commons/OSValidator.java
@@ -1,47 +1,32 @@
 package org.bbaw.bts.commons;
+
 public class OSValidator {
- 
-	private static String OS = System.getProperty("os.name").toLowerCase();
- 
-	public static void main(String[] args) {
- 
-		System.out.println(OS);
- 
-		if (isWindows()) {
-			System.out.println("This is Windows");
-		} else if (isMac()) {
-			System.out.println("This is Mac");
-		} else if (isUnix()) {
-			System.out.println("This is Unix or Linux");
-		} else if (isSolaris()) {
-			System.out.println("This is Solaris");
-		} else {
-			System.out.println("Your OS is not support!!");
-		}
-	}
- 
-	public static boolean isWindows() {
- 
-		return (OS.indexOf("win") >= 0);
- 
-	}
- 
-	public static boolean isMac() {
- 
-		return (OS.indexOf("mac") >= 0);
- 
-	}
- 
-	public static boolean isUnix() {
- 
-		return (OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") > 0 );
- 
-	}
- 
-	public static boolean isSolaris() {
- 
-		return (OS.indexOf("sunos") >= 0);
- 
-	}
- 
+
+    public enum OSType { WINDOWS, MAC, LINUX };
+
+	public static String osName;
+	public static OSType osType;
+	static {
+	    osName = System.getProperty("os.name");
+	    String foo = osName.toLowerCase();
+	    if (foo.contains("win"))
+	        osType = OSType.WINDOWS;
+	    else if (foo.contains("mac"))
+	        osType = OSType.MAC;
+	    else
+	        osType = OSType.LINUX;
+    };
+
+    public static boolean isWindows() {
+        return osType == OSType.WINDOWS;
+    }
+
+    public static boolean isMac() {
+        return osType == OSType.MAC;
+    }
+
+    public static boolean isUnix() {
+        // FIXME Rename to isLinux
+        return osType == OSType.LINUX;
+    }
 }
