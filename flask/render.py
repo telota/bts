748e52e34afb6bd0b6cd4f95d29959e27dd772ac
Sebastian Götte <bbaw@jaseg.net>
2018-06-25 15:29:51 +0200

    Add copyright statements to everything

--- flask/render.py
+++ flask/render.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+#
+# Lightweight Berlin Text System Search Interface
+# Copyright 2018 Sebastian Götte/Berlin Brandenburg Academy of Sciences <bbaw@jaseg.net, sgoette@bbaw.de>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sqlite3
 import json
 import hashlib
@@ -25,6 +42,7 @@ render_mdc_cache = SimpleCache()
 
 app = Flask(__name__)
 app.config.from_envvar('BTSRENDER_SETTINGS')
+app.jinja_env.line_comment_prefix = '##'
 
 
 class ObjectType(Enum):
--- flask/text_content.py
+++ flask/text_content.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+#
+# Lightweight Berlin Text System Search Interface
+# Copyright 2018 Sebastian Götte/Berlin Brandenburg Academy of Sciences <bbaw@jaseg.net, sgoette@bbaw.de>
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#
+
 import json
 import hashlib
 import re
